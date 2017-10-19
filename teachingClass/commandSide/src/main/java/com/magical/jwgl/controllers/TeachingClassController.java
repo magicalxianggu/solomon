900f007e00a756310b7257a2ebfc1c44be41fdea
magicalXiangGu <529206001@qq.com>
2017-10-19 16:33:23 +0800

    modify

--- teachingClass/commandSide/src/main/java/com/magical/jwgl/controllers/TeachingClassController.java
+++ teachingClass/commandSide/src/main/java/com/magical/jwgl/controllers/TeachingClassController.java
@@ -1,21 +1,14 @@
 package com.magical.jwgl.controllers;
 
-
 import com.magical.cloud.commands.CreateTeachingClassCommand;
-import com.magical.cloud.domain.TeacherPositionEnum;
 import org.axonframework.commandhandling.CommandExecutionException;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -29,20 +22,11 @@ public class TeachingClassController {
     private CommandGateway commandGateway;
 
     @PostMapping(value = "/addTeachingClass")
-    public void addTeachingClass(Model model, @RequestParam HashMap<String, String> paramsMap){
+    public void addTeachingClass( CreateTeachingClassCommand command){
 
         try{
 
-            HashMap<String,String> courseInfo = new HashMap<>();
-            String studingInterval = paramsMap.get("studingInterval");
-            Map<String,TeacherPositionEnum> teachers = new HashMap<>();
-            ArrayList<String> studentIDs = new ArrayList<>();
-
-            //封装命名数据
-
-            CreateTeachingClassCommand command = new CreateTeachingClassCommand(courseInfo,studingInterval,teachers,studentIDs);
-
-            //发送命令
+               //发送命令
             commandGateway.sendAndWait(command);
 
         }catch (CommandExecutionException cex){
